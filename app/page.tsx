0cfeca98a28c7c13b55d4a6a85e5a354da24cca0
skarakash <karakash.sergey@gmail.com>
2024-04-05 22:13:53 +0300

    fallback page

--- app/page.tsx
+++ app/page.tsx
@@ -2,24 +2,34 @@
 import Sidebar from "@/components/Sidebar";
 import NewInvoiceButton from "@/components/NewInvoiceButton";
 import Filter from "@/components/Filter";
-import { useRef, useEffect, useState } from 'react';
+import Image from "next/image";
+import empty from '@/public/empty.svg'
+import { useEffect, useState } from 'react';
 
+interface Invoice {
+  id: string
+  status: string
+  paymentDue: string
+  clientName: string
+  total: number
+}
 export default function Home() {
   const [selectedTypes, setSelectedTypes] = useState<string[]>([])
+  const [invoices, setInvoices] = useState<Invoice[]>([])
 
-  
+
   useEffect(() => {
     console.log(selectedTypes)
   }, [selectedTypes])
 
   return (
-    <div>
+    <div className="h-screen flex flex-col">
       <Sidebar />
-      <div className="ml-[24px] mr-[24px]">
+      <div className="ml-[24px] mr-[24px] grow flex flex-col">
         <div className="flex items-center">
           <div>
             <h1 className="text-headings font-bold leading-[22px] text-[24px] tracking-[-0.75] mb-[7px]">Inovices</h1>
-            <p className="text-subheadings font-medium leading-[15px] text-[13px] tracking-[-0.1px]">7 Inovices</p>
+            {invoices.length == 0 && <p className="text-subheadings font-medium leading-[15px] text-[13px] tracking-[-0.1px]">No invoices</p>}
           </div>
           <div className="ml-auto">
             <Filter
@@ -31,7 +41,21 @@ export default function Home() {
             <NewInvoiceButton />
           </div>
         </div>
+        {invoices.length === 0 && <div className="grow flex flex-col justify-center mt-[0] m-auto text-center">
+          <Image
+            src={empty}
+            width={193}
+            height={160}
+            alt="no items image"
+            className="mb-[42px] md:w-[240px] md:h-[200px]"
+          />
+          <h2 className="font-bold leading-[22px] text-[24px] tracking-[-0.75px] mb-[23px]">There is nothing here</h2>
+          <p className="text-subheadings leading-[15px] text-[13px] tracking-[-0.1px]">Create an invoice by clicking the</p>
+          <p className="text-subheadings leading-[15px] text-[13px] tracking-[-0.1px]"><span className="font-bold">New&nbsp;</span>button and get started</p>
+        </div>
+        }
       </div>
     </div>
   );
 }
+
